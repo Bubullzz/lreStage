0200728aa047be69694e98a5f0845fb75959da16
rayan.mostovoi <rayan.mostovoi@epita.fr>
2024-11-22 10:53:10 +0100

    updadet sourcesExtractor script ^^

--- sourcesExtractor/src/main.rs
+++ sourcesExtractor/src/main.rs
@@ -1,22 +1,28 @@
 use std::path::PathBuf;
 use swh_graph::graph::*;
-use swh_graph::properties::Maps;
 use swh_graph::NodeType;
+use swh_graph::mph::DynMphf;
 
 fn main()
 {
     println!("starting program");
-    let basename = PathBuf::from("/work/rayan.mostovoi/historyMachin/graph");
+    println!("loading graph...");
+    let graph = swh_graph::graph::SwhUnidirectionalGraph::new(PathBuf::from("/masto/2024-05-16/compressed/graph"))
+    .expect("Could not load graph")
+    .init_properties()
+    .load_properties(|properties| properties.load_maps::<DynMphf>())
+    .expect("Could not load SWHID<->node id maps");
 
-    println!("loading graph... this might take A WHILE (20 minutes for le petit teaser python)");
-    let graph = swh_graph::graph::load_full::<swh_graph::mph::DynMphf>(basename)
-        .expect("Could not load graph");
-        println!("right here 3");
     println!("graph loaded !! :D");
+    println!("starting to map !! :D");
+
 
     let origin_nodes: Vec<usize> = (0..graph.num_nodes())
     .filter(|&node| graph.properties().node_type(node) == NodeType::Origin)
     .collect();
+
+    println!("starting to print !! :D");
+
 
     for node in origin_nodes {
         let swhid = graph.properties().swhid(node);
